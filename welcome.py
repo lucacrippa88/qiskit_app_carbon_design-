b62952d1adec3c7e56fcce7dd1e25305a55e345b
Luca Crippa <luca.crippa88@gmail.com>
2019-06-14 15:22:17 +0200

    Login system commented (on welcome.py)

--- welcome.py
+++ welcome.py
@@ -51,92 +51,97 @@ print('finished printing')
 app = flask.Flask(__name__)
 jsonify = flask.jsonify
 
-# === Start Login ====
-app.secret_key = 'italy ibm q application pythonQ'
-login_manager = flask_login.LoginManager()
-login_manager.init_app(app)
-# Mock database
-users = {credentials_file['user']['mail']: {'password': credentials_file['user']['password']}}
-class User(flask_login.UserMixin):
-    pass
+# # === Start Login ====
+# app.secret_key = 'italy ibm q application pythonQ'
+# login_manager = flask_login.LoginManager()
+# login_manager.init_app(app)
+# # Mock database
+# users = {credentials_file['user']['mail']: {'password': credentials_file['user']['password']}}
+# class User(flask_login.UserMixin):
+#     pass
+#
+# @login_manager.user_loader
+# def user_loader(email):
+#     if email not in users:
+#         return
+#
+#     user = User()
+#     user.id = email
+#     return user
+#
+# @login_manager.request_loader
+# def request_loader(request):
+#     email = request.form.get('email')
+#     if email not in users:
+#         return
+#
+#     user = User()
+#     user.id = email
+#
+#     # DO NOT ever store passwords in plaintext and always compare password
+#     # hashes using constant-time comparison!
+#     user.is_authenticated = request.form['password'] == users[email]['password']
+#
+#     return user
+#
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if flask.request.method == 'GET':
+#         return app.send_static_file('login.html')
+#
+#     email = flask.request.form['email']
+#     if email in users.keys():
+#         if flask.request.form['password'] == users[email]['password']:
+#             user = User()
+#             user.id = email
+#             flask_login.login_user(user)
+#             return app.send_static_file('dashboard.html') # Redirect to landing page
+#
+#     return '''<p>Invalid login, try again</p>'''
+#
+# @login_manager.unauthorized_handler
+# def unauthorized_handler():
+#     return app.send_static_file('login.html')
+#
+# # === End Login ===
 
-@login_manager.user_loader
-def user_loader(email):
-    if email not in users:
-        return
-
-    user = User()
-    user.id = email
-    return user
-
-@login_manager.request_loader
-def request_loader(request):
-    email = request.form.get('email')
-    if email not in users:
-        return
-
-    user = User()
-    user.id = email
-
-    # DO NOT ever store passwords in plaintext and always compare password
-    # hashes using constant-time comparison!
-    user.is_authenticated = request.form['password'] == users[email]['password']
-
-    return user
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if flask.request.method == 'GET':
-        return app.send_static_file('login.html')
-
-    email = flask.request.form['email']
-    if email in users.keys():
-        if flask.request.form['password'] == users[email]['password']:
-            user = User()
-            user.id = email
-            flask_login.login_user(user)
-            return app.send_static_file('dashboard.html') # Redirect to landing page
-
-    return '''<p>Invalid login, try again</p>'''
-
-@login_manager.unauthorized_handler
-def unauthorized_handler():
-    return app.send_static_file('login.html')
-
-# === End Login ===
+# @app.route('/', methods=['GET', 'POST'])
+# @flask_login.login_required
+# def Welcome():
+#     return app.send_static_file('login.html')
 
 @app.route('/', methods=['GET', 'POST'])
-@flask_login.login_required
+# @flask_login.login_required
 def Welcome():
-    return app.send_static_file('login.html')
+    return app.send_static_file('dashboard.html')
 
 @app.route('/dashboard',methods=['GET', 'POST'])
-@flask_login.login_required
+# @flask_login.login_required
 def Dashboard():
     return app.send_static_file('dashboard.html')
 
 @app.route('/spheres', methods=['GET', 'POST'])
-@flask_login.login_required
+# @flask_login.login_required
 def Spheres():
     return app.send_static_file('spheres.html')
 
 @app.route('/docs', methods=['GET', 'POST'])
-@flask_login.login_required
+# @flask_login.login_required
 def Docs():
     return app.send_static_file('docs.html')
 
 @app.route('/team', methods=['GET', 'POST'])
-@flask_login.login_required
+# @flask_login.login_required
 def Team():
     return app.send_static_file('team.html')
 
 @app.route('/publications', methods=['GET', 'POST'])
-@flask_login.login_required
+# @flask_login.login_required
 def Publications():
     return app.send_static_file('publications.html')
 
 @app.route('/emoticons', methods=['GET', 'POST'])
-@flask_login.login_required
+# @flask_login.login_required
 def Emoticons():
     return app.send_static_file('emoticons.html')
 
